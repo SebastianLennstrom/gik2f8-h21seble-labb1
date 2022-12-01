5704295fbf852f7b9a938bdacddfbd20a0491b58
Sebastian Lennström <sebastian.lennstrom1@gmail.com>
2022-12-01 22:42:23 +0100

    test comit

---
+++ Inlämning js/MyJs/myJs.js
@@ -0,0 +1,29 @@
+/*const target = (BookListItem) => {
+    BookLis
+    const html = `
+    <div> 
+        <header>
+        Title of book
+        </header>
+        <p>
+        This is information about the book
+        </p>
+  </div>
+    `
+
+    return html
+
+
+ 
+}*/
+
+/*const hiddenItem = (book) => {
+    let html = `<li
+                  class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                ${book.author} - ${book.title}    
+                </li>`;
+    return html;
+  };*/
+
+ 
+
