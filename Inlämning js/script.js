3fb5daa791cb57f6c500e7018c11ee0d20e86960
Sebastian Lennström <sebastian.lennstrom1@gmail.com>
2022-12-04 17:33:19 +0100

    laggt till bookListInfo

--- Inlämning js/script.js
+++ Inlämning js/script.js
@@ -32,7 +32,32 @@ function renderBookList(bookList) {
 
 
 
+const bookListInfo = (book) => {
+  let html = `  <div id="hidden-Item" style =visability:visible;background-color:white;border-radius:10px;z-index:100;position:fixed;left:50%;top:50%;transform:translate(-50%,-50%);max-width:40%> 
+    <header style = padding:10px,15px;display:flex;align-items:center;justify-content:center;transition:200msease-in-out>
+    ${book.title} </header>
+    <div style = padding:1rem>
+    ${book.coverImage} 
+    </div>
+  </div>`;
+return html;
+}
 
+function renderToolBox(){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 const createPopupWindow = () => {
 
   let html = `  <div id="hidden-Item" style =visability:visible;background-color:white;border-radius:10px;z-index:100;position:fixed;left:50%;top:50%;transform:translate(-50%,-50%);max-width:40%> 
@@ -65,7 +90,7 @@ bookList.forEach(hoverTarget => {
 });
 
 
-
+*/
 
 
 /*for (let i = 0; i < bookList.length; i++) {
